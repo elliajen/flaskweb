c1318cce753e2e557cc7a742883954965db4b77f
elliajen <dmstndhk123@naver.com>
2021-10-05 12:02:40 +0900

    소스예제

---
+++ example/start_app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 127.0.0.1
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 127.0.0.1/loopindex
+@app.route('/loopindex')
+def loopindex():
+    items = ['a', 'b', 'c', 'd']
+    return render_template('loopindex.html', items = items)
+
+# 127.0.0.1/node
+@app.route('/node')
+def node():
+    return render_template('node.html')
+
+@app.route('/nodeimg')
+def nodeimg():
+    return render_template('nodeimg.html')
+
+app.run()
--- member/static/js/main_js.js
+++ member/static/js/main_js.js
@@ -1,19 +1,21 @@
-/*메인 사진 바꾸기*/
-//이벤트가 2개인 경우 addEventListener() 함수 사용
+/* 사진 바꾸기 */
+// 이벤트가 2개 이상인 경우 - addEventListener() 함수 사용
 pic = document.getElementById('pic');
-//마우스을 올리고 내렸을때 이벤트
-pic.addEventListener('mouseover', function(){   //주의 on을 생략
-    pic.src = "../static/img/bg2.jpg";
-});
-pic.addEventListener('mouseout', function(){
-    pic.src = "../static/img/bg5.jpg";
+
+//마우스를 올리고 내렸을때 이벤트
+pic.addEventListener("mouseover", function(){ //주의 on을 생략
+    pic.src = "../static/images/coffee-pink.jpg";
 });
 
-/*시계 넣기*/
+pic.addEventListener("mouseout", function(){
+    pic.src = "../static/images/coffee-blue.jpg";
+});
+
+/* 디지털 시계 */
 setInterval(myWatch, 1000)
 
 function myWatch(){
     var date = new Date();
-    var now = date.toLocaleTimeString()'
+    var now = date.toLocaleTimeString();
     document.getElementById('demo').innerHTML = now;
 }
